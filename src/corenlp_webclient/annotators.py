dd6adfd9658dce751c2e976067f95e92de96d1b0
liu xue yan <liu_xue_yan@foxmail.com>
2019-03-08 18:47:02 +0800

    optimize

--- src/corenlp_webclient/annotators.py
+++ src/corenlp_webclient/annotators.py
@@ -16,21 +16,19 @@ class BaseAnnotator:  # pylint:disable=too-few-public-methods
 
     def __init__(self, options: Optional[BaseOptions] = None):
         self._options = options
-        self._options_dict = {}
+        self._options_dict = self.make_options_dict()
 
-    def get_options_dict(self) -> Dict[str, Any]:
-        if self._options_dict:
-            return self._options_dict
+    def make_options_dict(self) -> Dict[str, Any]:
         if self._options:
-            self._options_dict = {
+            return {
                 '.'.join([self.name, _snake_to_camel(k)]): v
                 for k, v in self._options.to_dict().items()
             }
-        return self._options_dict
+        return {}
 
     @property
     def options_dict(self) -> Dict[str, Any]:
-        return self.get_options_dict()
+        return self._options_dict
 
 
 class WordsToSentenceAnnotator(BaseAnnotator):  # pylint:disable=too-few-public-methods
